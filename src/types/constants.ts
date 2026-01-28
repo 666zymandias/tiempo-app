b7b3fadfddcf44f1a6ecb92edf556ad36dea18a0
Felipe Santos <hydfelipe@gmail.com>
2026-01-28 21:01:35 +0100

    Modularizadas constantes

--- src/types/constants.ts
+++ src/types/constants.ts
@@ -1,1 +1,11 @@
-export const actualWeather: number = 1;
+//APP Language
+export const LANG: string = "es";
+
+//API
+export const URL_BASE: string = "https://api.weatherapi.com/v1/";
+export const endpoints = {
+    CURRENT_WEATHER: "current.json",
+}
+
+export const ACTUAL_WEATHER: number = 1;
+
